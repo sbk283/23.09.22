1f2a5bf9e59d853fe1168fe1da352318785ebac5
wasabikun1704 <sbk283@naver.com>
2023-09-22 10:44:38 +0900

    firsh exam

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,43 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
-        a사람.달리기_속력 = 20;
-        a사람.달리다();
-        // 사람이 20km/h로 달립니다.
-        a사람.달리다();
-        // 사람이 20km/h로 달립니다.
+        로봇 로봇1 = new 로봇();
+        int i = 로봇.get정수();
+        boolean b = 로봇.get논리();
+        사람 a사람 = 로봇.get사람();
+        사람 a사람2 = 로봇.get사람2();
+        사람 a사람3 = 로봇.get사람3();
+        사람 a사람4 = 로봇.get사람4();
+        사람 a사람5 = 로봇.get사람5(123, false);
+        로봇.get사람6(123, false);
+    }
+}
 
-        a사람.달리기_속력 = 50;
-        a사람.달리다();
-        // 사람이 50km/h로 달립니다.
+class 로봇 {
+    static int get정수() {
+        return 1;
+    }
+    static boolean get논리 () {
+        return true;
+    }
+    static 사람 get사람() {
+        return new 사람();
+    }
+    static 사람 get사람2() {
+        return new 사람();
+    }
+    static 사람 get사람3() {
+        return new 사람();
+    }
+    static 사람 get사람4() {
+        return new 사람();
+    }
+    static 사람 get사람5(int a, boolean b) {
+        return new 사람();
+    }
+    static 사람 get사람6(int a, boolean b) {
+        return new 사람();
     }
 }
-class 사람 {
-    int 달리기_속력;
-    void 달리다(){
-        System.out.println("사람이 " + 달리기_속력 + "km/h로 달랍니다.");
-    }
-}
+
+class 사람 {}
